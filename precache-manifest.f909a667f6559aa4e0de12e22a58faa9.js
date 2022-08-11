fdaa3a4acd75b827a3ea6769d639be0a722e6724
alan-ortiz <alan@baldertelecom.net>
2022-08-11 00:24:10 -0500

    Updates

---
+++ precache-manifest.f909a667f6559aa4e0de12e22a58faa9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49259537c33e9e502b3d9a3fbc7d183",
+    "url": "/react-gposalinas-v2/index.html"
+  },
+  {
+    "revision": "47e143e367b4b07b42c4",
+    "url": "/react-gposalinas-v2/static/css/main.1f63eee1.chunk.css"
+  },
+  {
+    "revision": "4590d87bc4df49b66160",
+    "url": "/react-gposalinas-v2/static/js/2.b14ed5f4.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/react-gposalinas-v2/static/js/2.b14ed5f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e143e367b4b07b42c4",
+    "url": "/react-gposalinas-v2/static/js/main.cd102df8.chunk.js"
+  },
+  {
+    "revision": "685dd8f270ddc2d01a9c",
+    "url": "/react-gposalinas-v2/static/js/runtime-main.d78995d0.js"
+  },
+  {
+    "revision": "167cec923401b253d47618730c4e4d78",
+    "url": "/react-gposalinas-v2/static/media/Millennial Solstice.167cec92.otf"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/react-gposalinas-v2/static/media/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  },
+  {
+    "revision": "b560f479295ff53863487ecc19d67f9d",
+    "url": "/react-gposalinas-v2/static/media/appBackground.b560f479.jpg"
+  }
+]);
